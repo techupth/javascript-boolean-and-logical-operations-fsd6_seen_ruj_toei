0ece49402cb31e04df13d698c4b3bd968e95e5b5
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-05-09 14:12:49 +0700

    finish the exercise

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,9 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
 
-let isAllow;
+let isAllow = true;
 
 // Start coding here
+isAllow = !isAllow && hasCriminalBlacklist;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,18 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = false;
+let isPlatinum = false;
 
-let hasPromotion;
+let hasPromotion = true;
+hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    hasBoughtProductFromITCategory &&
+    hasAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
